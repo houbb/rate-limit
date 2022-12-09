8698790858da3332bb0c904ef5f280362bfe592e
binbin.hou <binbin.hou@huifu.com>
2022-12-09 10:37:54 +0800

    release branch 1.1.1

--- rate-limit-core/src/main/java/com/github/houbb/rate/limit/core/support/method/RateLimitMethodService.java
+++ rate-limit-core/src/main/java/com/github/houbb/rate/limit/core/support/method/RateLimitMethodService.java
@@ -1,8 +1,6 @@
 package com.github.houbb.rate.limit.core.support.method;
 
-import com.github.houbb.heaven.annotation.CommonEager;
-import com.github.houbb.heaven.constant.PunctuationConst;
-import com.github.houbb.heaven.util.lang.ObjectUtil;
+import com.github.houbb.heaven.util.lang.reflect.ReflectMethodUtil;
 import com.github.houbb.rate.limit.api.support.IRateLimitMethodService;
 
 import java.lang.reflect.Method;
@@ -16,31 +14,7 @@ public class RateLimitMethodService implements IRateLimitMethodService {
     @Override
     public String getMethodId(Method method, Object[] params) {
         // 是否需要包含 class?
-        return getMethodFullName(method);
-    }
-
-    /**
-     * 方法全名此处应该考虑不同的参数问题。
-     *
-     * @param method 方法
-     * @return 完整的方法名称
-     * @since 0.0.1
-     */
-    @CommonEager
-    private static String getMethodFullName(Method method) {
-        if(method == null) {
-            return "null";
-        }
-
-        final String className = method.getDeclaringClass().getName();
-        Class[] types = method.getParameterTypes();
-        StringBuilder nameBuilder = new StringBuilder(className + "." + method.getName());
-        if (ObjectUtil.isNotEmpty(types)) {
-            for (Class parameter : types) {
-                nameBuilder.append(PunctuationConst.COLON).append(parameter.getName());
-            }
-        }
-        return nameBuilder.toString();
+        return ReflectMethodUtil.getMethodFullName(method);
     }
 
 }
